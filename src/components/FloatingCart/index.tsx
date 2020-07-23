7bc219bb763c1fef1f6c25973107441e888bee42
Augusto <ahaavila@gmail.com>
2020-07-23 20:51:24 -0300

    Adicionado barra inferior, onde fica a quantidade de itens do carrinho e o valor total

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -23,16 +23,29 @@ const FloatingCart: React.FC = () => {
 
   const navigation = useNavigation();
 
+  // Função que exibe o valor total do carrinho
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      // vou salvar numa variavel o preço do produto multiplicado pela sua quantidade
+      const productsSubtotal = product.price * product.quantity;
 
-    return formatValue(0);
+      // retorno o accumulator + o subtotal
+      return accumulator + productsSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
+  // Função que vai exibir o total de itens no carinho
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      // vou salvar numa variavel a quantidade
+      const productsQuantity = product.quantity;
 
-    return 0;
+      return accumulator + productsQuantity;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
