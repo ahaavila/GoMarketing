21717a65e751de10c3ed41dd6ea876c25cb0b991
Augusto <ahaavila@gmail.com>
2020-07-23 20:52:18 -0300

    Adicionado funções que vão adicionar um item no carrinho, remover um item no carrinho. Mostrar o valor total e a quantidade de itens total do carrinho.

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -28,25 +28,98 @@ const CartContext = createContext<CartContext | null>(null);
 const CartProvider: React.FC = ({ children }) => {
   const [products, setProducts] = useState<Product[]>([]);
 
+  // Função para carregar os produtos
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      // carrego os produtos
+      const storagedProducts = await AsyncStorage.getItem(
+        '@GoMarketplace:products',
+      );
+
+      // Se existir alguma coisa, eu jogo ele pra dentro de setProducts
+      if (storagedProducts) {
+        setProducts([...JSON.parse(storagedProducts)]);
+      }
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  // Função para adicionar um item no carrinho
+  const addToCart = useCallback(
+    async product => {
+      // Verifico se o item seleciona já exista no carrinho
+      const productExists = products.find(p => p.id === product.id);
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      // Se existir o item no carrinho
+      if (productExists) {
+        setProducts(
+          // dou um map em todos os meus products no carrinho
+          products.map(p =>
+            // se o id do produto do carrinho for igual ao id do produto em questão eu somo 1 na quantidade, se não, eu só retorno o produto
+            p.id === product.id ? { ...product, quantity: p.quantity + 1 } : p,
+          ),
+        );
+      } else {
+        // Caso o item selecionado não estiver no carrinho, eu adiciono ele com a quantidade = 1
+        setProducts([...products, { ...product, quantity: 1 }]);
+      }
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      await AsyncStorage.setItem(
+        '@GoMarketplace:products',
+        JSON.stringify(products),
+      );
+    },
+    [products],
+  );
+
+  // Função que adicona uma quantidade a mais no carrinho
+  const increment = useCallback(
+    async id => {
+      // percorro todos os itens do meu carrinho
+      const newProducts = products.map(product =>
+        // se existir um item no carrinho que tenha o mesmo id do item que eu to aumentando, eu adiciono 1 na quantidade
+        // se não existir, eu só retorno o item.
+        product.id === id
+          ? { ...product, quantity: product.quantity + 1 }
+          : product,
+      );
+
+      // jogo a minha variavel para dentro do estado
+      setProducts(newProducts);
+
+      // atualizo meu storage com os novos dados
+      await AsyncStorage.setItem(
+        '@GoMarketplace:products',
+        JSON.stringify(newProducts),
+      );
+    },
+    [products],
+  );
+
+  // Função que remove uma quantidade do item no carrinho
+  const decrement = useCallback(
+    async id => {
+      // / percorro todos os itens do meu carrinho
+      const newProducts = products.map(product =>
+        // se existir um item no carrinho que tenha o mesmo id do item que eu to aumentando, eu removo 1 na quantidade
+        // se não existir, eu só retorno o item.
+        product.id === id
+          ? { ...product, quantity: product.quantity - 1 }
+          : product,
+      );
+
+      // jogo a minha variavel para dentro do estado
+      setProducts(newProducts);
+
+      // atualizo meu storage com os novos dados
+      await AsyncStorage.setItem(
+        '@GoMarketplace:products',
+        JSON.stringify(newProducts),
+      );
+    },
+    [products],
+  );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
